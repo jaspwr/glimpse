1cb37477be2a3a915ad98151f7b25782da3ea343
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-07-22 22:45:26 +1000

    feat: license

--- src/applet/preview_window.rs
+++ src/applet/preview_window.rs
@@ -1,3 +1,19 @@
+// Glimpse - GNU/Linux Launcher and File search utility.
+// Copyright (C) 2024 https://github.com/jaspwr
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::{
     path::PathBuf,
     sync::{Arc, Mutex},
@@ -228,4 +244,3 @@ pub struct SafeBox {
 
 unsafe impl Sync for SafeBox {}
 unsafe impl Send for SafeBox {}
-
